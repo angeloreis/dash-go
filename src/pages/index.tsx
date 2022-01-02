f081ceb57a0e7c01e52fa860725f3f612e415e82
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-01-02 19:24:03 -0300

    fix: useform on login form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import { Flex, Button, Stack } from "@chakra-ui/react";
 import { Input } from "../components/Form/Input";
 
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
 export default function SignIn() {
+
+  const { register, handleSubmit, formState } = useForm()
+
+  const { errors, isSubmitting } = formState
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async  (dataForm) => {
+    await new Promise(resolve => setTimeout(resolve,5000))
+    console.log(dataForm)
+  }
+
   return (
     <Flex
       w='100vw'
@@ -16,23 +33,29 @@ export default function SignIn() {
         p='8'
         borderRadius={8}
         flexDir='column'
+        onSubmit={handleSubmit(handleSignIn)}
         >
           <Stack spacing='4'>
               <Input
                 name='email'
                 type='email'                
                 label='E-mail'
+                error={errors.email}
+                {...register('email',{required: 'E-mail obrigatório'})}
                 />
               <Input
                 name='password'
                 type='password'
                 label='Senha'
+                error={errors.password}
+                {...register('password',{required: 'Senha é obrigatória'})}
                 />
             <Button
               type='submit'
               mt='6'
               colorScheme='pink'
-              size='lg'>Entrar</Button>
+              size='lg'
+              isLoading={isSubmitting}>Entrar</Button>
           </Stack>         
       </Flex>
     </Flex>
